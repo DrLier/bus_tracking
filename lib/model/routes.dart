d7e80909eba354870b06dac2ab17f7bab774303b
Nathan <reyhan.nathaniel.20@student.it.ittelkom-sby.ac.id>
2023-02-07 08:05:27 +0700

    barubaru

--- lib/model/routes.dart
+++ lib/model/routes.dart
@@ -1,7 +1,8 @@
-import 'package:email_password_login/screens/home_screen.dart';
+// import 'package:email_password_login/screens/home_screen.dart';
 import 'package:email_password_login/screens/login_screen.dart';
 import 'package:email_password_login/screens/splash_screen.dart';
 import 'package:email_password_login/screens/welcome_screen.dart';
+import 'package:email_password_login/Screens/main/home/home_screen.dart';
 
 const String welcomeRoute = "/welcome";
 const String homeRoute = "/home";
